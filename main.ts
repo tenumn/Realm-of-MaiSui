86f16ddabf528365607d1560299b90ea67f345b3
Tenumn <tenumn@outlook.com>
2022-09-20 06:40:33 +0800

    bug fix and add belief

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import {
+	Belief,
 	Building,
 	Leader,
 	Nation,
@@ -9,20 +10,28 @@ import {
 } from "unciv-mod-toolchain";
 
 Unciv(() => {
-	var GoldenStatue = new Building("Golden Statue")
-		.cost(1e8)
-		.isWonder(true, true)
-		.required("Monument");
+	new Belief("Hades", "Pantheon").unique(
+		new Unique().add("[+1 Gold] from every [Luxury resource]")
+	);
+
+	new Building("Golden Statue").cost(1e8).required("Monument");
 
 	var Economy = new VictoryType("Economy")
 		.setVictoryIntro("Build Golden Statue\nto win!")
-		.milestones("Build [Golden Statue]");
+		.milestones("Build [Golden Statue]")
+		.setVictoryText(
+			"The golden statue you built towered in the center of the city to declare your wealth and status to the world!"
+		)
+		.setDefeatText("You defeat!");
 
-	var MaiSui = new Leader("MaiSui", Economy).unique(
-		new Unique("Trade Reform").add("[+100]% [Gold] from Trade Routes")
-	);
-
-	var RealmOfMaiSui = new Nation("Realm of MaiSui", MaiSui)
+	var RealmOfMaiSui = new Nation(
+		"Realm of MaiSui",
+		new Leader("MaiSui", Economy).unique(
+			new Unique("Trade Reform")
+				.add("[+1 Gold] from each Trade Route")
+				.add("[Great Merchant] is earned [50]% faster")
+		)
+	)
 		.adjective("Chinese")
 		.setColor(new NationColor().inner(64, 191, 191).outer(255, 255, 255))
 		.addCitie("MaiCity");
